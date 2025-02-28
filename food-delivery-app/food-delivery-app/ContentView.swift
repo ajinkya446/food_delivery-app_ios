4be696e34b58950be659c869a666a10faa73a6d3
ajinkya446 <ajinkya446@gmail.com>
2025-02-28 12:23:25 +0530

    Adding project setup and structure.

---
+++ food-delivery-app/food-delivery-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  food-delivery-app
+//
+//  Created by Ajinkya Aher on 28/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
